3fc96ceb8335251bd1a0fe94f7f7843a73128a10
kevinkiklee <kevin.kik.lee@gmail.com>
2026-05-11 18:29:09 -0400

    style(c1): biome auto-format batch-pipeline test imports

--- system/tests/integration/biographer-batch-pipeline.test.js
+++ system/tests/integration/biographer-batch-pipeline.test.js
@@ -4,10 +4,7 @@ import { tmpdir as __robinTmpdir } from 'node:os';
 import { join as __robinJoin, resolve } from 'node:path';
 import { test } from 'node:test';
 import { surql } from 'surrealdb';
-import {
-  biographerProcess,
-  biographerProcessBatch,
-} from '../../cognition/biographer/pipeline.js';
+import { biographerProcess, biographerProcessBatch } from '../../cognition/biographer/pipeline.js';
 import { writeConfig as __robinWriteConfig } from '../../config/paths.js';
 import { close, connect } from '../../data/db/client.js';
 import { runMigrations } from '../../data/db/migrate.js';
